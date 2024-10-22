3a3f16247744563458c054161c59f5022f0dae1f
Qianni Zhang <zhangqianni24@gmail.com>
2024-10-23 04:43:13 +0800

    add, delete professors function added.

---
+++ src/professors/professorSlice.js
@@ -0,0 +1,43 @@
+import api from "../app/api";
+
+const professorApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getProfessors: build.query({
+      query: () => "/professors",
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["Professor"],
+    }),
+    getProfessor: build.query({
+      query: (id) => "/professors/" + id,
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["Professor"],
+    }),
+    addProfessor: build.mutation({
+      query: (professor) => ({
+        url: "/professors",
+        method: "POST",
+        body: professor,
+      }),
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      invalidatesTags: ["Professor"],
+    }),
+    deleteProfessor: build.mutation({
+      query: (id) => ({
+        url: "/professors/" + id,
+        method: "DELETE",
+      }),
+      transformErrorResponse: (response) => response.data.error,
+      invalidatesTags: ["Professor"],
+    }),
+  }),
+});
+
+export const {
+  useGetProfessorsQuery,
+  useGetProfessorQuery,
+  useAddProfessorMutation,
+  useDeleteProfessorMutation,
+} = professorApi;

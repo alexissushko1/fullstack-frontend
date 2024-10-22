4979cedfa823fb2ba8f3cb5080e0391d260515a8
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-22 13:20:42 -0400

    Integrate RTK Query Architecture

---
+++ src/app/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  return <></>;
+}
---
+++ src/app/api.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      token && headers.set("authorization", `Bearer ${token}`);
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+});
+
+export default api;

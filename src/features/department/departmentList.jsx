9fdb622df88ee86a119119c2905ad013669296ae
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-23 14:35:37 -0400

    Updated so that when I click see details, I can see my update Form

--- src/features/department/departmentList.jsx
+++ src/features/department/departmentList.jsx
@@ -2,6 +2,8 @@ import { useGetDepartmentsQuery } from "./departmentSlice";
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import DepartmentForm from "./departmentForm";
+import "./DepartmentList.css";
 
 export default function DepartmentList(/*{ setSelectedDepartmentId }*/) {
   const { data: departments = [], isLoading } = useGetDepartmentsQuery();
@@ -15,23 +17,30 @@ export default function DepartmentList(/*{ setSelectedDepartmentId }*/) {
   return (
     <article>
       <h1>Departments</h1>
-      <ul className="departments">
-        {isLoading && <li>Loading departments...</li>}
-        {departments.map((department) => (
-          <li key={department.id}>
-            <h2>{department.name}</h2>
-            <figure>
-              <h3>{department.description}</h3>
-              <h2>{department.email}</h2>
-              <h2>{department.phone}</h2>
-              <img src={department.imageUrl} alt={department.name} />
-            </figure>
-            <button onClick={() => handleSeeDetails(department.id)}>
-              See Details
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="departmentGroup">
+        <div className="listOfDepartments">
+          <ul className="departments">
+            {isLoading && <li>Loading departments...</li>}
+            {departments.map((department) => (
+              <li key={department.id}>
+                <h2>{department.name}</h2>
+                <figure>
+                  <h3>{department.description}</h3>
+                  <h2>{department.email}</h2>
+                  <h2>{department.phone}</h2>
+                  <img src={department.imageUrl} alt={department.name} />
+                </figure>
+                <button onClick={() => handleSeeDetails(department.id)}>
+                  See Details
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="departmentForm">
+          <DepartmentForm />
+        </div>
+      </div>
     </article>
   );
 }
